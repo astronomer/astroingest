0ad95ad5a70bb0421bde7e5c82cbb43ccf26d337
iancmoritz <ian@alkeri.com>
2024-08-15 21:57:27 -0400

    attempt to get clickhouse working but it isnt

--- dev/dags/dag_rest_api_pokemon_dlt_abstracted.py
+++ dev/dags/dag_rest_api_pokemon_dlt_abstracted.py
@@ -4,6 +4,7 @@ from airflow.decorators import dag
 import dlt
 from dlt.common import pendulum
 from airflow.operators.empty import EmptyOperator
+from dlt.sources.credentials import ConnectionStringCredentials
 
 from astroingest.dlt_pipeline_task_group import DltPipelineTaskGroup
 
@@ -30,11 +31,12 @@ def astroingest_load_data():
     Same as the dag_rest_api_pokemon DAG, but written with DltPipelineTaskGroup to abstract the dlt pipeline creation.
     """
     from include.rest_api import pokemon_source
+    from include.sql_database import sql_database
 
     pre_dlt = EmptyOperator(task_id="pre_dlt")
 
-    dlt_task_group = DltPipelineTaskGroup(
-        pipeline_name="astroingest_rest_api_pipeline_pokemon",
+    dlt_task_group_pg = DltPipelineTaskGroup(
+        pipeline_name="astroingest_postgres_rest_api_pipeline_pokemon",
         dlt_source=pokemon_source(),
         dataset_name="pokemon",
         destination=dlt.destinations.postgres(
@@ -44,9 +46,25 @@ def astroingest_load_data():
         wipe_local_data=True,
     )
 
+    credentials = ConnectionStringCredentials(
+        "postgresql://airflow:pg_password@postgres:5432/airflow"
+    )
+    postgres_source = sql_database(credentials, "pokemon").with_resources("pokemon")
+
+    dlt_task_group_clickhouse = DltPipelineTaskGroup(
+        pipeline_name="astroingest_clickhouse_rest_api_pipeline_pokemon",
+        dlt_source=postgres_source,
+        dataset_name="pokemon",
+        destination=dlt.destinations.clickhouse(
+            "http://airflow:clickhouse_password@clickhouse:9000/airflow?secure=0"
+        ),
+        use_data_folder=False,
+        wipe_local_data=True,
+    )
+
     post_dlt = EmptyOperator(task_id="post_dlt")
 
-    pre_dlt >> dlt_task_group >> post_dlt
+    (pre_dlt >> dlt_task_group_pg >> dlt_task_group_clickhouse >> post_dlt)
 
 
 astroingest_load_data()
